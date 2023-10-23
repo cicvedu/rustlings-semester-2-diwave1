f94d0ca7275bd4ade9f74f2fd41df33f529e6e62
diwave <diwave@qq.com>
2023-10-23 22:59:52 +0800

    update 50

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,49 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        match scores.get(&team_1_name) {
+            None => {
+                let team = Team {
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                };
+                scores.insert(team_1_name, team);
+            },
+            Some(team) => {
+                let mut team: Team = Team {
+                    goals_scored: team.goals_scored,
+                    goals_conceded: team.goals_conceded,
+                };
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+                if let Some(x) = scores.get_mut(&team_1_name) {
+                    *x = team;
+                }
+            }
+        }
+
+        match scores.get(&team_2_name) {
+            None => {
+                let team = Team {
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                };
+                scores.insert(team_2_name, team);
+            },
+            Some(team) => {
+                let mut team = Team {
+                    goals_scored: team.goals_scored,
+                    goals_conceded: team.goals_conceded,
+                };
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+                if let Some(x) = scores.get_mut(&team_2_name) {
+                    *x = team;
+                }
+            }
+        }
+
     }
     scores
 }
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let out = input.to_string().clone();
+    out.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let appended = format!("{} {}", input, "world!".repeat(1));
+    appended
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let out = input.to_string().clone();
+    out.replace("cars", "balloons")
 }
 
 #[cfg(test)]
